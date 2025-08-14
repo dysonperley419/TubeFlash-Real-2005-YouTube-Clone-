9d4b1581f22f3c0b5cac6eef511a11f8926ea50e
dysonperley419 <perleydyson903@gmail.com>
2025-08-14 19:41:19 -0300

    Update config.php

--- TubeFlash_files/public_html/config.php
+++ TubeFlash_files/public_html/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', 'mysql.serv00/ct8pl or any');
-define('DB_USERNAME', 'yourusername');
-define('DB_PASSWORD', 'yourpassword');
-define('DB_NAME', 'yourdbname');
+define('DB_SERVER', 'mysql2.serv00.com');
+define('DB_USERNAME', 'm11265_dexptuba3');
+define('DB_PASSWORD', 'Dysonp2013');
+define('DB_NAME', 'm11265_2005tube');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
@@ -12,4 +12,5 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
 ?>
