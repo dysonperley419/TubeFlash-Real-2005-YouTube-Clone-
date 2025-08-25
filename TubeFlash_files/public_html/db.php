71a925556b430730c1ef80d00296b7b3f813bf3a
dysonperley419 <perleydyson903@gmail.com>
2025-08-24 23:44:40 -0300

    Update db.php

--- TubeFlash_files/public_html/db.php
+++ TubeFlash_files/public_html/db.php
@@ -1,3 +1,3 @@
 <?php
-$connect = mysqli_connect('mysql2.serv00.com', 'm11265_dexptuba3', 'Dysonp2013', 'm11265_dexptuba3') or die ("Database error!"); // db, do not remove or broken site.
+$connect = mysqli_connect('mysql2.serv00.com', 'm11265_dexptuba3', 'Dysonp2013', 'm11265_tubeflash') or die ("Database error!"); // db, do not remove or broken site.
 ?>
